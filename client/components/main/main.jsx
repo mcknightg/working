7c9167ece6e5d1614fac95aef3ab3a3ee9711687
georgemcknight <rsmrallen88@gmail.com>
2016-02-13 10:59:33 -0500

    #4.4 Ads

--- client/components/main/main.jsx
+++ client/components/main/main.jsx
@@ -1,5 +1,7 @@
 Main = React.createClass({
     render(){
+        var adobj = {_id:1,text:'My First Ad',title:'Some Company',image:'http://placehold.it/150x150'};
+
         return (
             <div className="col-sm-9 col-sm-11" id="main">
                 <div>
@@ -11,7 +13,7 @@ Main = React.createClass({
                                 <button className="btn btn-md">More</button>
                             </div>
                             <div className="col-sm-3">
-                                Ads
+                                <Ad ad={adobj}/>
                             </div>
                         </div>
                     </div>
